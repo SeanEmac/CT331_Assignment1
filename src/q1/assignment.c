c33eac56408b77075c8d3ae7c334752446810235
seanemac <S.mccann11@nuigalway.ie>
2018-09-26 19:29:59 +0100

    Question1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int a;
+int* b;
+long c;
+double* d;
+char** e;
+
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+
+  printf("int: %ld \n", sizeof(a));
+  printf("int*: %ld \n", sizeof(b));
+  printf("long: %ld \n", sizeof(c));
+  printf("double: %ld \n", sizeof(d));
+  printf("char**: %ld \n", sizeof(e));
+
   return 0;
 }
